795f88cd954cade6f11d4ff68a983522e9cd5bd9
Viraat <viraat.laldas@gmail.com>
2025-02-02 22:42:49 -0800

    update

--- examples/clip/example_clip.py
+++ examples/clip/example_clip.py
@@ -3,7 +3,6 @@ from exla.models.clip import clip
 
 clip = clip()
 
-
 predictions = clip.inference(
     image_paths=["data/dog.png", "data/cat.png"],
     classes=["a dog", "a cat", "a bird"]
--- exla/models/clip/_implementations/clip_cpu.py
+++ exla/models/clip/_implementations/clip_cpu.py
@@ -1,6 +1,6 @@
 from ._base import Clip_Base
 
-class Clip_CPU(Clip_base):
+class Clip_CPU(Clip_Base):
     def __init__(self, model_name="openai/clip-vit-base-patch32"):
         """
         Initializes a CLIP model on Orin Nano with TensorRT-like optimizations.
